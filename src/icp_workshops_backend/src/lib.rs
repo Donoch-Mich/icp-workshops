f02f457dd4a6cb35713f5dd513d16ac88320bb6f
Donoch-Mich Donoch <Donochmich3@gmail.com>
2025-03-20 11:28:46 +0000

    calulator

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -2,3 +2,17 @@
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+fn calculate(a: i32, b: i32, operaltor: String) -> String{
+    let result = match operaltor.as_str(){
+        "+"=> Some(a+b),
+        "-"=> Some(a-b),
+        "*"=> Some(a*b),
+        "/"=> if b != 0 {Some(a/b)} else {None},
+            _ => None
+            };
+            match result { 
+                Some(value) => format!("Result: {}", value),
+                None => "invalide operator or division by zero".to_string(),
+
+            } 
+}
